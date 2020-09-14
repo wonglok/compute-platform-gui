6382737e9e9882e03f9e9089b3c7cebb96a2dd70
wonglok <yellowhappy831@gmail.com>
2020-09-15 05:53:26 +0800

    save

---
+++ src/components/WebGL/AppUIs/EditorWorkBoxTypes/plane-buffer-geometry/export-index.js
@@ -0,0 +1,111 @@
+// export default as { Files } from
+import fileTree from './loadFiles.js'
+import coverImage from './img/thumb.svg'
+import { getID } from '../../../Core/O3DNode.js'
+
+const workBoxFrameColor = '#bababa'
+const workBoxScreenColor = '#ffffff'
+
+const io = {
+  inputs: [],
+  outputs: []
+}
+
+const guiData = {
+  width: 200,
+  height: 200,
+  segmentX: 70,
+  segmentY: 70
+}
+
+const compatability = {
+  boxIn: [
+  ],
+  boxOut: [
+    'draw-type'
+  ]
+}
+
+const displayName = 'Plane Buffer Geometry'
+
+const tags = [
+  'geometry',
+  'plane-buffer-geometry'
+]
+
+const gui = {
+  settings: 'PlaneBuffereGeometryGUI'
+}
+
+const buttons = {
+  tl: false,
+  tr: {},
+  bl: {},
+  br: {
+    icon: 'circle-out',
+    mouseMode: 'box-out'
+  },
+  br2: false
+  // br2: {
+  //   icon: require('./img/circle-out.svg'),
+  //   mouseMode: 'box-out'
+  // }
+
+  // br: {
+  //   icon: require('./img/circle-out.svg'),
+  //   mouseMode: 'box-out'
+  // }
+}
+
+export {
+  displayName,
+  tags,
+  fileTree,
+  coverImage,
+  workBoxScreenColor,
+  workBoxFrameColor,
+  gui,
+  guiData,
+  io,
+  buttons,
+  compatability
+}
+
+/**
+ *
+ * extraUniforms: [
+    {
+      name: 'distributionMode',
+      type: 'float',
+      updater: 'select',
+      options: [
+        {
+          _id: getID(),
+          value: '0.0',
+          display: 'Cube Distribution'
+        },
+        {
+          _id: getID(),
+          value: '1.0',
+          display: 'Plane Distribution'
+        }
+      ],
+      needsAuhtorises: false,
+      value: '0.0'
+    },
+    {
+      name: 'faceDistribution',
+      type: 'float',
+      updater: 'slider',
+      needsAuhtorises: false,
+      value: 3.0973
+    },
+    {
+      name: 'faceSize',
+      type: 'float',
+      updater: 'slider',
+      needsAuhtorises: false,
+      value: 0.3084
+    }
+  ]
+ */
--- src/components/WebGL/AppUIs/EditorWorkBoxTypes/plane-buffer-geometry/src/monitor/monitor.js
+++ src/components/WebGL/AppUIs/EditorWorkBoxTypes/plane-buffer-geometry/src/monitor/monitor.js
@@ -1,36 +1,36 @@
-export const use = ({ box, work }) => {
+export const use = async ({ box, work }) => {
   // if (box && box.userData && box.userData.work && box.userData.work.coverImage) {
-  let { MeshBasicMaterial, LineSegments, PlaneBufferGeometry, Color, DoubleSide } = box.deps.THREE
+  let { MeshBasicMaterial, Mesh, PlaneBufferGeometry, Color, DoubleSide } = box.deps.THREE
   let { scene, camera } = box
   // let texture = new TextureLoader().load(box.userData.work.coverImage)
   // let mat = new MeshBasicMaterial({ map: texture, color: new Color('#ffffff'), side: DoubleSide })
   // let geo = new PlaneBufferGeometry(170, 170, 2, 2)
 
-  // let directionaLight = new PointLight(0xffffff, 1, 1500, 2)
-  // directionaLight.position.x = 300
-  // directionaLight.position.y = 300
-  // directionaLight.position.z = 300
-  // scene.add(directionaLight)
-
-  let geo = new PlaneBufferGeometry(120, 120, 12, 12)
   let mat = new MeshBasicMaterial({ wireframe: true, color: new Color('#bebebe'), side: DoubleSide })
-  let mesh = new LineSegments(geo, mat)
+  let mesh = new Mesh(undefined, mat)
 
   camera.position.z = 150
   scene.background = new Color('#ffffff')
 
   box.onLoop(() => {
-    mesh.rotation.z += 0.01
+    // mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.01
   })
 
-  scene.add(mesh)
+  box.onRefresh(() => {
+    let gui = work.guiData
+    let geo = new PlaneBufferGeometry(gui.width, gui.height, gui.segmentX, gui.segmentY)
+    mesh.geometry = geo
+    mesh.geometry.needsUpdate = true
+  })
+  box.runRefresh()
 
+  scene.add(mesh)
   // console.log(scene)
 
   box.onClean(() => {
-    geo.dispose()
+    mesh.geometry.dispose()
     mat.dispose()
     console.log('clean up')
   })
-  // }
 }
